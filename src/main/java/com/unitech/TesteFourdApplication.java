9b236cd6fd78125409f27f25ea3fe796c8187b06
Geraldo <geraldo.gja@gmail.com>
2023-07-04 10:57:53 -0300

    primeiro commit

---
+++ src/main/java/com/unitech/TesteFourdApplication.java
@@ -0,0 +1,13 @@
+package com.unitech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteFourdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteFourdApplication.class, args);
+	}
+
+}
