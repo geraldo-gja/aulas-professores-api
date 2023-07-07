d0ed888f4ad13cfe66dea2faabd48c6a49d39ad3
Geraldo <geraldo.gja@gmail.com>
2023-07-07 18:55:34 -0300

    Spring-Security - refatoração anotações ProfessorController

--- src/main/java/com/unitech/controller/ProfessorController.java
+++ src/main/java/com/unitech/controller/ProfessorController.java
@@ -1,29 +1,28 @@
 package com.unitech.controller;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.HashMap;
 
 //importes para validação e tratamento de erro por request
 import javax.validation.Valid;
-import org.springframework.validation.FieldError;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.unitech.config.security.FilterToken;
 import com.unitech.entity.Professor;
 import com.unitech.service.IProfessorService;
 
